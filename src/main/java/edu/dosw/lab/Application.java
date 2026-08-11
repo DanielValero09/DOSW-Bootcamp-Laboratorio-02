6ab68dc2cc94cc953dbbe8d74f2d3588b83cb73b
ChanguaLover4ever <danivillacast06@gmail.com>
2026-08-11 15:37:48 -0500

    feature: Preparación - Subir Estructura de Paquetes del Laboratorio 02

---
+++ src/main/java/edu/dosw/lab/Application.java
@@ -0,0 +1,16 @@
+// Application.java
+package edu.dosw.lab; 
+
+public class Application { 
+
+    public static void main(String[] args) { 
+
+        System.out.println( 
+
+            "Proyecto Maven configurado y corriendo correctamente" 
+
+        ); 
+
+    } 
+
+}
