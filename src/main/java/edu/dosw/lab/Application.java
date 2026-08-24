845e7944fea8dc1401d0ac10a2726e9d2122e8c0
DanielValero09 <daniel.valero-q@mail.escuelaing.edu.co>
2026-08-23 22:04:34 -0500

    Problema resuelto de importar los retos en la clase Application

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -2,7 +2,10 @@
 package edu.dosw.lab;
 
 import edu.dosw.lab.Solid.Reto1.Reto1;
+import edu.dosw.lab.comportamiento.reto4.Reto4;
+import edu.dosw.lab.creacionales.reto2.Reto2;
 import edu.dosw.lab.creacionales.reto3.Reto3;
+import edu.dosw.lab.estructurales.reto5.Reto5;
 
 public class Application {
 
